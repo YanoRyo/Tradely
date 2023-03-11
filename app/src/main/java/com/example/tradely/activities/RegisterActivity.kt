02f7d2c5960865750e9370855bd05fbd0b76aba0
YanoRyo <yanoryo7032@gmail.com>
2023-03-11 16:52:11 +0900

    Validating Register Entries

--- app/src/main/java/com/example/tradely/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/tradely/activities/RegisterActivity.kt
@@ -2,13 +2,14 @@ package com.example.tradely.activities
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.example.tradely.R
 import com.example.tradely.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
 
@@ -29,9 +30,78 @@ class RegisterActivity : AppCompatActivity() {
             )
         }
 
+        setupActionBar()
+
         binding.tvLogin.setOnClickListener {
             val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
             startActivity(intent)
         }
+        binding.btnRegister.setOnClickListener {
+            validateRegisterDetails()
+        }
     }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarRegisterActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+        binding.toolbarRegisterActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_confirm_password),
+                    true
+                )
+                false
+            }
+
+            binding.etPassword.text.toString()
+                .trim { it <= ' ' } != binding.etPassword.text.toString().trim { it <= ' ' } -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),
+                    true
+                )
+                false
+            }
+            !binding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_agree_terms_and_condition),
+                    true
+                )
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.register_successful), false)
+                true
+            }
+        }
+    }
+
 }
