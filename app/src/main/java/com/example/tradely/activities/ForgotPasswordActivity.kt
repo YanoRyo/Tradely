aaec4aff6af7e4f33038ed4b9c063b5c764e0fce
YanoRyo <yanoryo7032@gmail.com>
2023-03-12 00:49:32 +0900

    The Forgot Password Impletemtaion and Test

--- app/src/main/java/com/example/tradely/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/tradely/activities/ForgotPasswordActivity.kt
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.tradely.R
 import com.example.tradely.databinding.ActivityForgotPasswordBinding
-class ForgotPasswordActivity : AppCompatActivity() {
+import com.google.firebase.auth.FirebaseAuth
+
+class ForgotPasswordActivity : BaseActivity() {
 
     private lateinit var binding: ActivityForgotPasswordBinding
 
@@ -40,6 +43,41 @@ class ForgotPasswordActivity : AppCompatActivity() {
         }
         binding.toolbarForgotPasswordActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            // Get the email id from the input field.
+            val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+
+            // Now, If the email entered in blank then show the error message or else continue with the implemented feature.
+            if (email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            } else {
+
+                // Show the progress dialog.
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                // This piece of code is used to send the reset password link to the user's email id if the user is registered.
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+
+                        // Hide the progress dialog
+                        hideProgressDialog()
+
+                        if (task.isSuccessful) {
+                            // Show the toast message and finish the forgot password activity to go back to the login screen.
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
         }
     }
 }
