3a64eb61ef9f51b14d6c35bf27acfb4a91bebbe2
YanoRyo <yanoryo7032@gmail.com>
2023-03-12 13:07:29 +0900

    Setting up Firestore

---
+++ app/src/main/java/com/example/tradely/firestore/FirestoreClass.kt
@@ -0,0 +1,10 @@
+package com.example.tradely.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
+class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+}
