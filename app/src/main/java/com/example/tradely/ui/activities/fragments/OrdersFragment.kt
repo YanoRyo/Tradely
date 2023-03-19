e32e61b469a9b3cfd7c737c48c354d6a377c6215
YanoRyo <yanoryo7032@gmail.com>
2023-03-19 14:02:33 +0900

    Taking Care of the Different Fragment

--- app/src/main/java/com/example/tradely/ui/activities/fragments/OrdersFragment.kt
+++ app/src/main/java/com/example/tradely/ui/activities/fragments/OrdersFragment.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.tradely.databinding.FragmentHomeBinding
+import com.example.tradely.databinding.FragmentOrdersBinding
 
-class HomeFragment : Fragment() {
+class OrdersFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentOrdersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,12 +19,12 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-//        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val textView: TextView = binding.textHome
-        textView.text = "This is home Fragment"
+        val textView: TextView = binding.textNotifications
+            textView.text = "This is notifications Fragment"
 
         return root
     }
--- app/src/main/java/com/example/tradely/ui/activities/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/tradely/ui/activities/fragments/ProductsFragment.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.tradely.databinding.FragmentNotificationsBinding
+import com.example.tradely.databinding.FragmentProductsBinding
 
-class NotificationsFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProductsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,12 +19,12 @@ class NotificationsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-//        val notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
+//        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val textView: TextView = binding.textNotifications
-            textView.text = "This is notifications Fragment"
+        val textView: TextView = binding.textHome
+        textView.text = "This is home Fragment"
 
         return root
     }
