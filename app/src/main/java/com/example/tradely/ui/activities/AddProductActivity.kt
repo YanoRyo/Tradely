dd6d800f996487dcdd3fe1427c104c2c05cf48c7
YanoRyo <yanoryo7032@gmail.com>
2023-03-21 00:48:57 +0900

    Let the User Select a Product Image

--- app/src/main/java/com/example/tradely/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/example/tradely/ui/activities/AddProductActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tradely.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.tradely.R
 import com.example.tradely.databinding.ActivityAddProductBinding
+import com.example.tradely.utils.Constants
+import com.example.tradely.utils.GlideLoader
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+class AddProductActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAddProductBinding
 
@@ -16,6 +26,8 @@ class AddProductActivity : AppCompatActivity() {
         setContentView(view)
 
         setupActionBar()
+
+        binding.ivAddUpdateProduct.setOnClickListener(this)
     }
 
     /**
@@ -32,5 +44,73 @@ class AddProductActivity : AppCompatActivity() {
         }
 
         binding.toolbarAddProductActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null){
+            when(view.id){
+                // The permission code is similar to the user profile image selection.
+                R.id.iv_add_update_product -> {
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Constants.showImageChooser(this@AddProductActivity)
+                    } else {
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+                        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE)
+                    }
+                }
+
+//                R.id.btn_submit -> {
+//                    if (validateProductDetails()) {
+//
+//                        uploadProductImage()
+//                    }
+//                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    binding.ivAddUpdateProduct.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_vector_edit))
+
+                    val selectedImageFileURL = data.data!!
+                    try {
+                        GlideLoader(this).loadUserPicture(selectedImageFileURL, binding.ivProductImage)
+                    }catch (e:IOException){
+                        e.printStackTrace()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // A log is printed when user close or cancel the image selection.
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
     }
 }
