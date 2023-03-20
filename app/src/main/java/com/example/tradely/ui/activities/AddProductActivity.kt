e254a69d195ed55deb0a4af118355acee92e7606
YanoRyo <yanoryo7032@gmail.com>
2023-03-21 00:07:42 +0900

    Designing the Add Product Activity

--- app/src/main/java/com/example/tradely/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/example/tradely/ui/activities/AddProductActivity.kt
@@ -2,6 +2,7 @@ package com.example.tradely.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tradely.R
 import com.example.tradely.databinding.ActivityAddProductBinding
 
 class AddProductActivity : AppCompatActivity() {
@@ -13,5 +14,23 @@ class AddProductActivity : AppCompatActivity() {
         binding = ActivityAddProductBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setupActionBar()
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarAddProductActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        binding.toolbarAddProductActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
