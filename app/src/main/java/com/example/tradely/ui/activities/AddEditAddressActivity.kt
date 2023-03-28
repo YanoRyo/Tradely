f87d82f067bd3027effc7a72df07965e5b418122
YanoRyo <yanoryo7032@gmail.com>
2023-03-28 23:31:43 +0900

    Checking user entries as well as prepare the Address Datamodel

--- app/src/main/java/com/example/tradely/ui/activities/AddEditAddressActivity.kt
+++ app/src/main/java/com/example/tradely/ui/activities/AddEditAddressActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tradely.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import com.example.tradely.R
 import com.example.tradely.databinding.ActivityAddEditAddressBinding
+import com.example.tradely.firestore.FirestoreClass
+import com.example.tradely.models.Address
+import com.example.tradely.utils.Constants
 
-class AddEditAddressActivity : AppCompatActivity() {
+class AddEditAddressActivity : BaseActivity() {
 
     private lateinit var binding: ActivityAddEditAddressBinding
 
@@ -29,5 +32,88 @@ class AddEditAddressActivity : AppCompatActivity() {
         }
 
         binding.toolbarAddEditAddressActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function to save the address to the cloud firestore.
+     */
+    private fun saveAddressToFirestore() {
+
+        // Here we get the text from editText and trim the space
+        val fullName: String = binding.etFullName.text.toString().trim { it <= ' ' }
+        val phoneNumber: String = binding.etPhoneNumber.text.toString().trim { it <= ' ' }
+        val address: String = binding.etAddress.text.toString().trim { it <= ' ' }
+        val zipCode: String = binding.etZipCode.text.toString().trim { it <= ' ' }
+        val additionalNote: String = binding.etAdditionalNote.text.toString().trim { it <= ' ' }
+        val otherDetails: String = binding.etOtherDetails.text.toString().trim { it <= ' ' }
+
+        if (validateData()) {
+
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val addressType: String = when {
+                binding.rbHome.isChecked -> {
+                    Constants.HOME
+                }
+                binding.rbOffice.isChecked -> {
+                    Constants.OFFICE
+                }
+                else -> {
+                    Constants.OTHER
+                }
+            }
+
+            val addressModel = Address(
+                FirestoreClass().getCurrentUserID(),
+                fullName,
+                phoneNumber,
+                address,
+                zipCode,
+                additionalNote,
+                addressType,
+                otherDetails
+            )
+        }
+    }
+
+    private fun validateData(): Boolean {
+        return when {
+
+            TextUtils.isEmpty(binding.etFullName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_full_name),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(binding.etPhoneNumber.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_phone_number),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(binding.etAddress.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_address), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etZipCode.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+
+            binding.rbOther.isChecked && TextUtils.isEmpty(
+                binding.etZipCode.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
     }
 }
