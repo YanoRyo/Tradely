c6da52ab5bed981667f788574da8d364a0ee1ac5
YanoRyo <yanoryo7032@gmail.com>
2023-04-08 14:57:01 +0900

    Finishing the Sold Products Details and the App

--- app/src/main/java/com/example/tradely/ui/activities/SoldProductDetailsActivity.kt
+++ app/src/main/java/com/example/tradely/ui/activities/SoldProductDetailsActivity.kt
@@ -2,9 +2,14 @@ package com.example.tradely.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.tradely.R
 import com.example.tradely.databinding.ActivitySoldProductDetailsBinding
 import com.example.tradely.models.SoldProduct
 import com.example.tradely.utils.Constants
+import com.example.tradely.utils.GlideLoader
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SoldProductDetailsActivity : AppCompatActivity() {
 
@@ -16,9 +21,71 @@ class SoldProductDetailsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        setupActionBar()
+
         var productDetails: SoldProduct = SoldProduct()
         if (intent.hasExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS)) {
             productDetails = intent.getParcelableExtra<SoldProduct>(Constants.EXTRA_SOLD_PRODUCT_DETAILS)!!
         }
+
+        setupUI(productDetails)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarSoldProductDetailsActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+        binding.toolbarSoldProductDetailsActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    /**
+     * A function to setup UI.
+     *
+     * @param productDetails Order details received through intent.
+     */
+    private fun setupUI(productDetails: SoldProduct) {
+
+        binding.tvOrderDetailsId.text = productDetails.order_id
+
+        // Date Format in which the date will be displayed in the UI.
+        val dateFormat = "dd MMM yyyy HH:mm"
+        // Create a DateFormatter object for displaying date in specified format.
+        val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.timeInMillis = productDetails.order_date
+        binding.tvOrderDetailsDate.text = formatter.format(calendar.time)
+
+        GlideLoader(this@SoldProductDetailsActivity).loadProductPicture(
+            productDetails.image,
+            binding.ivProductItemImage
+        )
+        binding.tvProductItemName.text = productDetails.title
+        binding.tvProductItemPrice.text ="$${productDetails.price}"
+        binding.tvSoldProductQuantity.text = productDetails.sold_quantity
+
+        binding.tvSoldDetailsAddressType.text = productDetails.address.type
+        binding.tvSoldDetailsFullName.text = productDetails.address.name
+        binding.tvSoldDetailsAddress.text =
+            "${productDetails.address.address}, ${productDetails.address.zipCode}"
+        binding.tvSoldDetailsAdditionalNote.text = productDetails.address.additionalNote
+
+        if (productDetails.address.otherDetails.isNotEmpty()) {
+            binding.tvSoldDetailsOtherDetails.visibility = View.VISIBLE
+            binding.tvSoldDetailsOtherDetails.text = productDetails.address.otherDetails
+        } else {
+            binding.tvSoldDetailsOtherDetails.visibility = View.GONE
+        }
+        binding.tvSoldDetailsMobileNumber.text = productDetails.address.mobileNumber
+
+        binding.tvSoldProductSubTotal.text = productDetails.sub_total_amount
+        binding.tvSoldProductShippingCharge.text = productDetails.shipping_charge
+        binding.tvSoldProductTotalAmount.text = productDetails.total_amount
     }
 }
--- app/src/main/java/com/example/tradely/ui/fragments/SoldProductsFragment.kt
+++ app/src/main/java/com/example/tradely/ui/fragments/SoldProductsFragment.kt
@@ -1,7 +1,6 @@
 package com.example.tradely.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,16 +12,6 @@ import com.example.tradely.firestore.FirestoreClass
 import com.example.tradely.models.SoldProduct
 import com.example.tradely.ui.adapters.SoldProductsListAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SoldProductsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SoldProductsFragment : BaseFragment() {
 
     private var _binding: FragmentSoldProductsBinding? = null
@@ -32,9 +21,10 @@ class SoldProductsFragment : BaseFragment() {
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sold_products, container, false)
+    ): View {
+        _binding = FragmentSoldProductsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
 
     override fun onResume() {
